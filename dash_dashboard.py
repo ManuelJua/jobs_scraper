318869a857c80e15ee6eb7a3984c692bf43fb46a
ManuelJua <manuelalejandrojuarez@gmail.com>
2024-10-18 02:10:40 +0100

    update .gitignore

--- dash_dashboard.py
+++ dash_dashboard.py
@@ -31,7 +31,8 @@ def filter_df(df, keywords):
     # patterns = [f'(?=.*{keyword})' for keyword in keywords.split()]
     # full_pattern = ''.join(patterns)
 
-    filtered_df = df[df['jobDescription'].str.contains(pat=keywords, regex=True, case=False)]
+    filtered_df = df[(df['jobDescription'].str.contains(pat=keywords, regex=True, case=False)) & (df['meanSalary']>1000)]
+    
    
     return filtered_df
 
