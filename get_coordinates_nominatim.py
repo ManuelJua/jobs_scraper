c5125f35772d93d169d519e1c9c40de23ab0e064
ManuelJua <manuelalejandrojuarez@gmail.com>
2024-11-03 11:23:38 +0000

    Add sync and async methods to GetCoordinatesNominatim

--- get_coordinates_nominatim.py
+++ get_coordinates_nominatim.py
@@ -1,4 +1,6 @@
+from geopy.adapters import AioHTTPAdapter
 from geopy.geocoders import Nominatim
+import asyncio
 from time import sleep
 import pandas as pd
 
@@ -7,8 +9,9 @@ class GetCoordinatesNominatim:
     
     def __init__(self):
         """Initialize the Nominatim geocoder with a user agent."""
-        self.app = Nominatim(user_agent='jobs')
         self.add_location_geocoded = {}
+        self.df:pd.DataFrame
+        self.location_df:pd.Series
 
     def _open_location_file(self, location_file_name:str):
         """Open the location file as a DataFrame."""
@@ -27,25 +30,60 @@ class GetCoordinatesNominatim:
         combined_df = pd.concat([self.location_df, dict_df], ignore_index=True)
         combined_df.to_csv('location_new.csv', index=False)
 
+    async def _async_geocode_address(self, location:str):
+        """Geocode a single address."""
+        try:
+            async with Nominatim(
+                user_agent="jobs_scraper",
+                adapter_factory=AioHTTPAdapter,
+                ) as geolocator:
+                geocode_result = await geolocator.geocode(location, timeout=None)
+                if geocode_result:
+                    # Extract latitude and longitude from the geocode result
+                    self.add_location_geocoded[location] = (geocode_result.latitude, geocode_result.longitude)
+                    print(location,geocode_result.latitude,geocode_result.longitude)
+                    self._save_location_dataframe()
+        except Exception as e:
+            print(f"Error geocoding location {location}: {e}")
+
+    async def _async_geocode_dataframe(self):
+        """Geocode all addresses in the DataFrame with missing lat/lon."""
+        unique_locations_missing = self.df[self.df['latitude'].isna()]['locationName'].unique()
+        tasks=[asyncio.create_task(self._geocode_address(location)) for location in unique_locations_missing]
+        await asyncio.gather(*tasks)
+            
     def _geocode_address(self, location:str):
         """Geocode a single address."""
         try:
-            geocode_result = self.app.geocode(location)
+            geolocator=Nominatim(user_agent="jobs_scraper")
+        
+            geocode_result = geolocator.geocode(location, timeout=None)
             if geocode_result:
                 # Extract latitude and longitude from the geocode result
                 self.add_location_geocoded[location] = (geocode_result.latitude, geocode_result.longitude)
                 print(location,geocode_result.latitude,geocode_result.longitude)
                 self._save_location_dataframe()
+            else:
+                print(f"Coordinates not found for {location}")
         except Exception as e:
             print(f"Error geocoding location {location}: {e}")
 
-    def _geocode_dataframe(self, df:pd.DataFrame):
+    def _geocode_dataframe(self):
         """Geocode all addresses in the DataFrame with missing lat/lon."""
-        unique_locations_missing = df[df['latitude'].isna()]['locationName'].unique()
+        unique_locations_missing = self.df[self.df['latitude'].isna()]['locationName'].unique()
         for location in unique_locations_missing:
             self._geocode_address(location)
-
-    def geocode_dataframe(self, location_file_name:str, df:pd.DataFrame):
+      
+    def async_geocode_dataframe(self, location_file_name:str, df:pd.DataFrame):
         """Public method to geocode addresses and save results."""
         self._open_location_file(location_file_name)
-        self._geocode_dataframe(df)
+        self.df=df
+        asyncio.run(self._geocode_dataframe())
+    
+    def geocode_dataframe(self, location_file_name:str, df:pd.DataFrame):
+        """Public method to geocode addresses and save results."""
+        print("Starting geocoding...")
+        self._open_location_file(location_file_name)
+        self.df=df
+        self._geocode_dataframe()
+
