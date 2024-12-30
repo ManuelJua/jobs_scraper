649df05341f20728032738d756f25e0a31fc8a03
ManuelJua <manuelalejandrojuarez@gmail.com>
2024-12-30 16:55:26 +0000

    Reorganize files

---
+++ etl/load_to_db.py
@@ -0,0 +1,42 @@
+import os
+from dotenv import load_dotenv
+import datetime
+import logging
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from ReedProcessor import Database, FileManager, DataPreparer
+
+load_dotenv()
+logging.basicConfig(level=logging.INFO)
+
+def main():
+    connection_string = os.getenv('DATABASE_URL')
+    db = Database(connection_string)
+    file_manager = FileManager()
+    data_preparer = DataPreparer()
+
+    
+    logging.info("Retrieving ids from database")
+    text_query="""SELECT id FROM jobs;"""
+    existent_data=db.query(query=text_query)
+    logging.info("Reading new data")
+    new_data = file_manager.get_parquet_file()
+    df_to_upload=data_preparer.filter_new_rows(existent_db=existent_data,new_data=new_data)
+    logging.info(f"{df_to_upload.shape[0]} new jobs available")
+    logging.info("Cleaning data")
+    df_to_upload=data_preparer.transform_data(df=df_to_upload)
+    logging.info("Preparing data to upload")
+    df_to_upload.to_csv('data_to_upload.csv',na_rep="",float_format="%.0f",index=False)
+    logging.info("Starting to upload data")
+    db.copy_from_file_to_db(csv_file='data_to_upload.csv')
+    logging.info("Data upload finished")
+    for file_name in os.listdir():
+        if file_name.endswith(".parquet"):
+            os.remove(file_name)
+            logging.info(f"{file_name} removed")
+    
+
+start = datetime.datetime.now()
+main()
+finish = datetime.datetime.now()
+print(f"Script duration {(finish-start).seconds} seconds")
---
+++ geocoding/get_coordinates_nominatim.py
@@ -0,0 +1,92 @@
+from geopy.adapters import AioHTTPAdapter
+from geopy.geocoders import Nominatim
+import asyncio
+from time import sleep
+import pandas as pd
+
+class GetCoordinatesNominatim:
+    """Class to handle geocoding using Nominatim."""
+    
+    def __init__(self):
+        """Initialize the Nominatim geocoder with a user agent."""
+        self.add_location_geocoded = {}
+        self.df:pd.DataFrame
+        self.location_df:pd.Series
+
+    def _open_location_file(self, location_file_name:str):
+        """Open the location file as a DataFrame."""
+        file_extension = location_file_name.split('.')[-1]
+        if file_extension == 'csv':
+            self.location_df = pd.read_csv(location_file_name)
+        elif file_extension == 'parquet':
+            self.location_df = pd.read_parquet(location_file_name)
+        else:
+            raise ValueError("Unsupported file format: {}".format(file_extension))
+
+    def _save_location_dataframe(self):
+        """Save the updated location DataFrame to a new CSV file."""
+        dict_df = pd.DataFrame(self.add_location_geocoded).T.reset_index()
+        dict_df.columns = ['location', 'latitude', 'longitude']
+        combined_df = pd.concat([self.location_df, dict_df], ignore_index=True)
+        combined_df.to_csv('location_new.csv', index=False)
+
+    async def _async_geocode_address(self, location:str):
+        """Geocode a single address."""
+        try:
+            async with Nominatim(
+                user_agent="jobs_scraper",
+                adapter_factory=AioHTTPAdapter,
+                ) as geolocator:
+                geocode_result = await geolocator.geocode(location, timeout=None)
+                if geocode_result:
+                    # Extract latitude and longitude from the geocode result
+                    self.add_location_geocoded[location] = (geocode_result.latitude, geocode_result.longitude)
+                    print(location,geocode_result.latitude,geocode_result.longitude)
+                    self._save_location_dataframe()
+        except Exception as e:
+            print(f"Error geocoding location {location}: {e}")
+
+    async def _async_geocode_dataframe(self):
+        """Geocode all addresses in the DataFrame with missing lat/lon."""
+        unique_locations_missing = self.df[self.df['latitude'].isna()]['location'].unique()
+        tasks=[asyncio.create_task(self._async_geocode_address(location)) for location in unique_locations_missing]
+        await asyncio.gather(*tasks)
+            
+    def _geocode_address(self, location:str):
+        """Geocode a single address."""
+        try:
+            geolocator=Nominatim(user_agent="jobs_scraper")
+        
+            geocode_result = geolocator.geocode(location, timeout=None)
+            if geocode_result:
+                # Extract latitude and longitude from the geocode result
+                self.add_location_geocoded[location] = (geocode_result.latitude, geocode_result.longitude)
+                print(location,geocode_result.latitude,geocode_result.longitude)
+                self._save_location_dataframe()
+            else:
+                print(f"Coordinates not found for {location}")
+        except Exception as e:
+            print(f"Error geocoding location {location}: {e}")
+
+    def _geocode_dataframe(self):
+        """Geocode all addresses in the DataFrame with missing lat/lon."""
+        unique_locations_missing = self.df[self.df['latitude'].isna()]['location'].unique()
+        for location in unique_locations_missing:
+            self._geocode_address(location)
+      
+    def async_geocode_dataframe(self, location_file_name:str, df:pd.DataFrame):
+        """Public method to geocode addresses and save results."""
+        print("Starting geocoding...")
+        self._open_location_file(location_file_name)
+        self.df=df
+        asyncio.run(self._async_geocode_dataframe())
+    
+    
+    def geocode_dataframe(self, location_file_name:str, df:pd.DataFrame):
+        """Public method to geocode addresses and save results."""
+        print("Starting geocoding...")
+        self._open_location_file(location_file_name)
+        self.df=df
+        self._geocode_dataframe()
+
+
